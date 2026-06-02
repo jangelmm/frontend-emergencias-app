464c3ca73f6b69f5698cb2cfdebc6ba18f2ef6f2
Baruc <3baruc69@gmail.com>
2026-06-02 00:19:02 -0600

    feat: update KPI 4 and KPI 5 widgets to improve interactivity and layout

--- lib/screens/widgets/kpi_4_comparativa_card.dart
+++ lib/screens/widgets/kpi_4_comparativa_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../screens/kpi/kpi_detail_screen.dart';
 
 class Kpi4Card extends StatelessWidget {
   final Map<String, dynamic> data;
@@ -9,52 +10,57 @@ class Kpi4Card extends StatelessWidget {
   Widget build(BuildContext context) {
     final isUp = data["tendencia"] == "ALZA";
 
-    return Card(
-      elevation: 4,
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              data["kpi"],
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) =>
+                const KpiDetailScreen(title: "KPI 1: Saturación", kpiId: 1),
+          ),
+        );
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            data["kpi"],
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
 
-            const SizedBox(height: 10),
+          const SizedBox(height: 10),
 
-            Text("Estado: ${data["estado"]}"),
-            Text("Año actual: ${data["anioActual"]} → ${data["totalActual"]}"),
-            Text(
-              "Año anterior: ${data["anioAnterior"]} → ${data["totalAnterior"]}",
-            ),
+          Text("Estado: ${data["estado"]}"),
+          Text("Año actual: ${data["anioActual"]} → ${data["totalActual"]}"),
+          Text(
+            "Año anterior: ${data["anioAnterior"]} → ${data["totalAnterior"]}",
+          ),
 
-            const SizedBox(height: 15),
+          const SizedBox(height: 15),
 
-            Row(
-              children: [
-                Icon(
-                  isUp ? Icons.arrow_upward : Icons.arrow_downward,
+          Row(
+            children: [
+              Icon(
+                isUp ? Icons.arrow_upward : Icons.arrow_downward,
+                color: isUp ? Colors.red : Colors.green,
+                size: 30,
+              ),
+              const SizedBox(width: 10),
+              Text(
+                "${data["porcentajeCambio"]}%",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                   color: isUp ? Colors.red : Colors.green,
-                  size: 30,
                 ),
-                const SizedBox(width: 10),
-                Text(
-                  "${data["porcentajeCambio"]}%",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: isUp ? Colors.red : Colors.green,
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
+          ),
 
-            const SizedBox(height: 5),
+          const SizedBox(height: 5),
 
-            Text(isUp ? "Incremento interanual" : "Disminución interanual"),
-          ],
-        ),
+          Text(isUp ? "Incremento interanual" : "Disminución interanual"),
+        ],
       ),
     );
   }
--- lib/screens/widgets/kpi_5_dias_criticos.dart
+++ lib/screens/widgets/kpi_5_dias_criticos.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import '../../screens/kpi/kpi_detail_screen.dart';
 
 class Kpi5Chart extends StatelessWidget {
   final List<dynamic> data;
@@ -8,42 +9,65 @@ class Kpi5Chart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 300,
-      child: BarChart(
-        BarChartData(
-          alignment: BarChartAlignment.spaceAround,
-          barGroups: data.asMap().entries.map((entry) {
-            final index = entry.key;
-            final item = entry.value;
+    return InkWell(
+      onTap: () {},
+      child: SizedBox(
+        height: 200,
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _title(), // 👈 aquí lo usas
 
-            return BarChartGroupData(
-              x: index,
-              barRods: [
-                BarChartRodData(
-                  toY: item["totalEmergencias"].toDouble(),
-                  width: 15,
-                  color: Colors.blue,
+            Expanded(
+              child: BarChart(
+                BarChartData(
+                  alignment: BarChartAlignment.spaceAround,
+                  barGroups: data.asMap().entries.map((entry) {
+                    final index = entry.key;
+                    final item = entry.value;
+
+                    return BarChartGroupData(
+                      x: index,
+                      barRods: [
+                        BarChartRodData(
+                          toY: item["totalEmergencias"].toDouble(),
+                          width: 15,
+                          color: Colors.blue,
+                        ),
+                      ],
+                    );
+                  }).toList(),
+
+                  titlesData: FlTitlesData(
+                    leftTitles: const AxisTitles(
+                      sideTitles: SideTitles(showTitles: false),
+                    ),
+                    bottomTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        showTitles: true,
+                        getTitlesWidget: (value, meta) {
+                          final dia = data[value.toInt()]["dia"];
+                          return Text(dia.substring(0, 3));
+                        },
+                      ),
+                    ),
+                  ),
                 ),
-              ],
-            );
-          }).toList(),
-
-          titlesData: FlTitlesData(
-            leftTitles: const AxisTitles(
-              sideTitles: SideTitles(showTitles: false),
-            ),
-            bottomTitles: AxisTitles(
-              sideTitles: SideTitles(
-                showTitles: true,
-                getTitlesWidget: (value, meta) {
-                  final dia = data[value.toInt()]["dia"];
-                  return Text(dia.substring(0, 3));
-                },
               ),
             ),
-          ),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _title() {
+    return const Padding(
+      padding: EdgeInsets.only(bottom: 16),
+      child: Text(
+        'KPI 5: Dias criticos',
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
       ),
     );
   }
